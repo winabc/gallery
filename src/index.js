a80019bb00d5fcabc1a2a34bbcbb38c73ee46680
winabc <ndiforaa@gmail.com>
2026-08-04 05:36:36 -1100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
+import {
+    SocketRoom
+} from "./socket.js";
 
+export default {
+async fetch(request,env){
+
+    const id =
+    env.SOCKET_ROOM.idFromName(
+        "main"
+    );
+    const room =
+    env.SOCKET_ROOM.get(id);
+    return room.fetch(request);
+}
+};
+export {
+    SocketRoom
+};
